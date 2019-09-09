bd66dd18616e480d8eaa7cc7e7778c151553c552
Carl Ortutay <carlorty@gmail.com>
2019-09-08 21:19:09 -0400

    made changes for Callback props assignment

--- src/card.js
+++ src/card.js
@@ -1,15 +1,22 @@
-import React from "react";
-import "./Card.css";
+import React from 'react';
+import './Card.css';
 
-function Card(props) {
+export default function Card(props) {
   return (
     <div className='Card'>
-      <button type='button'>delete</button>
+      <button
+        type='button'
+        onClick={() => props.onClickDelete(props.id)}
+      >
+        delete
+      </button>
       <h3>{props.title}</h3>
       <p>{props.content}</p>
     </div>
-  );
+  )
 }
 
-Card.defaultProps = { title: [] }
-export default Card;
+Card.propTypes = {
+  onClickDelete: () => {}
+}
+
